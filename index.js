04fc03dc86ce98490ad46bb807c3c73debe2dbdc
unknown <Katherine2bk@gmail.com>
2021-06-03 10:01:24 -0700

    created some notes in index.js, set up server listener, exported server from server.js with home endpoint, everything is working

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const server = require('./api/server');
 
 const port = 5000;
 
+server.listen(port, () => {
+    console.log(`\n server is running on port ${port} \n`)
+})
+
 // START YOUR SERVER HERE
+
+// 1. if you do not have a gitignore (npx gitignore node)
+// 2. set up your scripts in package.json "start" and "server"
+// 3. set up your eslint (npx eslint --init)
+// 4. require express
+// 5. create your server, set it to express
+// 6. create your port variable
+// 7. server.use(express.json())
+// 8. create the listener for your server
+// 9. set up your endpoints with statusCodes
+// 10. be sure to export your endpoints/server.js
+
+
